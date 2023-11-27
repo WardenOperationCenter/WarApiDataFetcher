3fb6dd1b6897ecb523de6940aacf210909a2d5b6
Loïc Schamber <loic.schamber@gmail.com>
2023-11-28 00:40:55 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Warden Operations Center
+WOC Is a tool aimed to give informations about War Status and facilitate the work of Warden commanders
+## Purpose
+
+## Run
+
+## Contribute
+
+## License
---
+++ test.mongodb.js
@@ -0,0 +1,53 @@
+
+use('WOC_3_Test');
+
+db.maps.deleteMany({})
+
+db.maps.insertMany([
+  {"_id": "TheFingersHex"},
+	{"_id": "GreatMarchHex"},
+	{"_id": "TempestIslandHex"},
+	{"_id": "MarbanHollow"},
+	{"_id": "ViperPitHex"},
+	{"_id": "BasinSionnachHex"},
+	{"_id": "StemaLandingHex"},
+	{"_id": "DeadLandsHex"},
+	{"_id": "HeartlandsHex"},
+	{"_id": "EndlessShoreHex"},
+	{"_id": "WestgateHex"},
+	{"_id": "OarbreakerHex"},
+	{"_id": "AcrithiaHex"},
+	{"_id": "MooringCountyHex"},
+	{"_id": "WeatheredExpanseHex"},
+	{"_id": "ReaversPassHex"},
+	{"_id": "LochMorHex"},
+	{"_id": "MorgensCrossingHex"},
+	{"_id": "StonecradleHex"},
+	{"_id": "AllodsBightHex"},
+	{"_id": "KalokaiHex"},
+	{"_id": "RedRiverHex"},
+	{"_id": "OriginHex"},
+	{"_id": "HowlCountyHex"},
+	{"_id": "ClahstraHex"},
+	{"_id": "ShackledChasmHex"},
+	{"_id": "SpeakingWoodsHex"},
+	{"_id": "TerminusHex"},
+	{"_id": "LinnMercyHex"},
+	{"_id": "ClansheadValleyHex"},
+	{"_id": "GodcroftsHex"},
+	{"_id": "NevishLineHex"},
+	{"_id": "CallumsCapeHex"},
+	{"_id": "FishermansRowHex"},
+	{"_id": "UmbralWildwoodHex"},
+	{"_id": "ReachingTrailHex"},
+	{"_id": "StlicanShelfHex"},
+	{"_id": "CallahansPassageHex"},
+	{"_id": "AshFieldsHex"},
+	{"_id": "KingsCageHex"},
+	{"_id": "DrownedValeHex"},
+	{"_id": "FarranacCoastHex"},
+	{"_id": "SableportHex"}
+])
+
+db.maps.countDocuments({})
+db.maps.find({})
